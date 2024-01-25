04907054128bbc5965aef6535537c52a7782aa40
uni <cbcb9995@naver.com>
2024-01-25 14:07:45 +0900

    commit5

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from weatherApp.views import index, check_weather
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +10,8 @@ urlpatterns = [
     path('', index, name='index'),  # index 함수를 불러옴
     path('check_weather/', check_weather, name='check_weather'),  # check_weather 함수를 불러옴
 ]
+
+urlpatterns += static(
+    prefix=settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
--- weatherApp/models.py
+++ weatherApp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class TravelSpot(models.Model):
     region = models.CharField(max_length=50)
     spot_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='travel_spot_images/', null=True, blank=True)
 
     def __str__(self):
         return f'{self.region} - {self.spot_name}'
